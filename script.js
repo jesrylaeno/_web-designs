65f9bddc10097c25ee7acc93ab46d2cbc3582aad
jesrylaeno <73213497+jesrylaeno@users.noreply.github.com>
2010-08-29 20:36:00 +0800

    017: fix index

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ function loadPage(url) {
   document.getElementById('mainFrame').src = url;
 }
 
-window.onload = () => loadPage('web-apps/dlspaceconstruction/index.html');
+window.onload = () => loadPage('web-apps/portfolio-01/index.html');
