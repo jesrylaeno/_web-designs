aa73b0f2d901b02cd141f7a5c1418259d4108f6c
jesrylaeno <73213497+jesrylaeno@users.noreply.github.com>
2020-11-29 14:30:00 +0800

    006: fix onload()

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ function loadPage(url) {
   document.getElementById('mainFrame').src = url;
 }
 
-window.onload = () => loadPage('web-apps/item1.html');
+window.onload = () => loadPage('web-apps/dlspaceconstruction/index.html');
